c5b19b82e1a796a58531d070db8379c15e9dfe40
Johni Martinez <80790359+Demasiado17@users.noreply.github.com>
2024-12-05 15:19:25 -0500

    Add files via upload
    Day 1 Project 
    
    Band Name Generator

---
+++ Day1Project.py
@@ -0,0 +1,10 @@
+print("Hello welcome to the brand name generator.\n")
+
+#Asking user to input a city then storing it into a variable called CityName
+CityName = input("What is the name of the city you grew up in?\n")
+
+#Asking the user for the name of a pet and storing it in a variable named Pet.
+Pet = input("What is your pet name if any?\n")
+
+#displaying final messge using both the variables created
+print("Your band name is "+CityName +" " +  Pet)
